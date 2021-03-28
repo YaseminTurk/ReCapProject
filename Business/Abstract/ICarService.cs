da80bf31d716c87de101686c917d7654a78e46ff
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-03-28 15:10:29 +0300

    GetCarsByBrandId , GetCarsByColorId Servisleri ve Add kuralları

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -8,5 +8,12 @@ namespace Business.Abstract
     public interface ICarService
     {
         List<Car> GetAll();
+        List<Car> GetCarsById(int id);
+        List<Car> GetCarsByBrandId(int id);
+        List<Car> GetCarsByColorId(int id);
+
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
     }
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -3,6 +3,7 @@ using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -14,9 +15,58 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car.CarName.Length>=2 && car.DailyPrice>0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                if (car.CarName.Length < 2 && car.DailyPrice <= 0)
+                {
+                    Console.WriteLine("Ekleme işlemi başarısız. Araba ismini en az 2 karakter ve araba günlük fiyatını 0'dan büyük giriniz.");
+                }
+                else if (car.CarName.Length < 2)
+                {
+                    Console.WriteLine("Ekleme işlemi başarısız. Araba ismini en az 2 karakter giriniz.");
+                }
+                else
+                {
+                    Console.WriteLine("Ekleme işlemi başarısız. Araba günlük fiyatını 0'dan büyük giriniz.");
+                }
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c=>c.BrandId==id).ToList();
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id).ToList();
+        }
+
+        public List<Car> GetCarsById(int id)
+        {
+            return _carDal.GetAll(c => c.CarId == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
