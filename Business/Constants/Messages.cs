01a2eeaf14649c2da49860b643484fa061602adb
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-03-30 16:03:08 +0300

    Entity oluşturma ve CRUD operasyonları

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,9 +6,11 @@ namespace Business.Constants
 {
     public static class Messages
     {
-        public static string CarAdded = "Ürün eklendi";
-        public static string CarNameAndDailyPriceInvalid = "Ekleme işlemi başarısız. Araba ismini en az 2 karakter ve araba günlük fiyatını 0'dan büyük giriniz.";
-        public static string CarNameInvalid = "Ekleme işlemi başarısız. Araba ismini en az 2 karakter giriniz.";
-        public static string CarDailyPriceInvalid = "Ekleme işlemi başarısız. Araba günlük fiyatını 0'dan büyük giriniz.";
+        public static string CarAdded = "Araç eklendi";
+        public static string CarNameAndDailyPriceInvalid = "Ekleme işlemi başarısız. Araç ismini en az 2 karakter ve araç günlük fiyatını 0'dan büyük giriniz.";
+        public static string CarNameInvalid = "Ekleme işlemi başarısız. Araç ismini en az 2 karakter giriniz.";
+        public static string CarDailyPriceInvalid = "Ekleme işlemi başarısız. Araç günlük fiyatını 0'dan büyük giriniz.";
+        public static string CarNotFound = "Araç bulunamadı.";
+
     }
 }
