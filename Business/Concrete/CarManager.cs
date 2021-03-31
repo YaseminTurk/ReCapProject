6aa2c8c2144a401e59777357333d277b6340503c
Yasemin Türk <yasemin.turk.1996@gmail.com>
2021-03-31 16:36:22 +0300

    WebAPI katmanı ve IoC Container

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -51,7 +51,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Car>> GetAll()
         {
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll());
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.CarAdded);
         }
 
         public IDataResult<Car> GetById(int id)
